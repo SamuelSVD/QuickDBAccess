6e6c67d8b9819526154725917ed33585c2befff8
SamuelSVD <sam.svd95@gmail.com>
2023-05-04 01:36:33 -0400

    Increment version

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("samver.ca")]
-[assembly: AssemblyProduct("TrayDir")]
+[assembly: AssemblyProduct("QuickDBAccess")]
 [assembly: AssemblyCopyright("Copyright © Sam Vergara 2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.4.0")]
+[assembly: AssemblyVersion("0.0.5.0")]
 // [assembly: AssemblyFileVersion("0.0.1.*")]
